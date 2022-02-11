914d4196cef61b9a6d0967ba1a653316846a626f
Isekku <Sekoucisse1701@gmail.com>
2022-02-11 18:05:51 +0100

    Finalisation de la version alpha

---
+++ app/src/main/java/fr/isekku/remotors/Telecommande.java
@@ -0,0 +1,14 @@
+package fr.isekku.remotors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Telecommande extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telecommande);
+    }
+}
